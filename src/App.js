97b13d74dc9b092ca9ae0927b144974522a9350b
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-08-30 13:59:42 -0700

    React Prop first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PersonCard from "./components/PersonCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard
+        firstName="Mikey"
+        lastName="Navarro"
+        age={22}
+        hairColor="brown"
+      />
+      <PersonCard
+        firstName="Tommy"
+        lastName="Caulfield"
+        age={23}
+        hairColor="brown"
+      />
+      <PersonCard
+        firstName="Ben"
+        lastName="Watson"
+        age={24}
+        hairColor="blond"
+      />
+      <PersonCard
+        firstName="Cody"
+        lastName="Stuart"
+        age={23}
+        hairColor="brown"
+      />
+      <PersonCard
+        firstName="Jake"
+        lastName="Alspaw"
+        age={23}
+        hairColor="blond"
+      />
+      <PersonCard
+        firstName="Isaac"
+        lastName="Diaz"
+        age={23}
+        hairColor="black"
+      />
+      <PersonCard
+        firstName="Carlos"
+        lastName="Alatore"
+        age={23}
+        hairColor="black"
+      />
+      <PersonCard
+        firstName="Nolan"
+        lastName="Lanza"
+        age={22}
+        hairColor="blond"
+      />
     </div>
   );
 }
